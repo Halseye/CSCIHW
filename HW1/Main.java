886237760efd956bf1405e77ae4c04d23a2f0df9
Halseye <halseye@go.stockton.edu>
2021-12-02 22:13:31 -0500

    homework

--- HW1/Main.java
+++ HW1/Main.java
@@ -5,10 +5,19 @@ public class Main {
         System.out.println("Start of Main");
         // Create an object that will make a Pyramid (see Pyramid.java for more info)
 
+        Pyramid newPyramid = new Pyramid();
+        newPyramid.Output();
+
         // Create an object that implements a method which will take an name, age, and favorite color and will return a story (see Adlibs.java for more info)
+
+        Adlibs newStory = new Adlibs();
+        System.out.println(newStory.story("Katie", "26", "Pink"));
+        
 
         // Create an object that implements a method which will  accept a string, an integer, and a char and will replace the char in the string at the given integer (See Replace.java for more info)
         
+        Replace newReplace = new Replace();
+        System.out.println(newReplace.charReplace("I MAKE BREAD", 2, 'T')); 
 
     }
 }
